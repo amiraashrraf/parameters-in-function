4eb3f8d90c2fe02c331df835cb502b0470d178d8
amiraashrraf <168367228+amiraashrraf@users.noreply.github.com>
2024-08-11 23:06:08 +0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  int num1 = 5;
+  int num2 = 1;
+  int result = calculator(num1, num2);
+  print(result);
+}
+
+int calculator(number1, number2) {
+  int result = number1 + number2;
+  return result;
+}
